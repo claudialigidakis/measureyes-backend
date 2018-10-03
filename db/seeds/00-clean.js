cf23f5a69ea654a6f958d0ab6ca5383f13cbeb29
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-10-03 13:30:47 -0700

    updates made

--- db/seeds/00-clean.js
+++ db/seeds/00-clean.js
@@ -1,4 +1,4 @@
 exports.seed = function(knex, Promise) {
-  const tablesToClean = ['accounts','locations', 'video']
+  const tablesToClean = ['accounts', 'locations', 'video']
   return tablesToClean.reduce((acc, ele) => acc.then(() => knex(ele).del()), Promise.resolve())
 };

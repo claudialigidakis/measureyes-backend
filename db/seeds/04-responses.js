76206dcecac6d8359fefc6d41eb40bf51a586967
Claudia Ligidakis <30705905+claudialigidakis@users.noreply.github.com>
2018-10-09 13:03:17 -0700

    updated seeds

--- db/seeds/04-responses.js
+++ db/seeds/04-responses.js
@@ -2,7 +2,7 @@ exports.seed = function(knex, Promise) {
   return knex('responses').del()
     .then(function () {
       return knex('responses').insert([
-        {id: 1, video_id: 1, time: '19:25:47', faces: 2, persons: 2}
+        {id: 1, video_id: 1, unix_time: '1539115352', faces: 2, persons: 2}
       ]);
     })
     .then(() => {
